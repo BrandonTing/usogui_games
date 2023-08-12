ea8e2d3e426a498b4b9c47138cecaba600b2e449
Brandon Ting <brandon1221t@gmail.com>
2023-08-12 23:00:58 +0800

    extract new game logic

--- src/games/hangman.rs
+++ src/games/hangman.rs
@@ -30,64 +30,74 @@ pub struct Hangman {
     players: (Player, Player),
 }
 
+fn new_hangman_game(
+    joker_options: &Vec<usize>,
+    card_options: &Vec<usize>,
+    steps: usize,
+) -> (Player, Player) {
+    let mut rng: rand::rngs::ThreadRng = rand::thread_rng();
+
+    let init_joker = joker_options[rng.gen_range(0..joker_options.len())];
+    println!("joker this round: {}", init_joker);
+    // shuffle cards
+    let mut init_cards: Vec<_> = card_options
+        .clone()
+        .into_iter()
+        .flat_map(|x| {
+            vec![
+                PlayerCard {
+                    card_type: CardType::Number,
+                    number: x,
+                },
+                PlayerCard {
+                    card_type: CardType::Number,
+                    number: x,
+                },
+            ]
+        })
+        .collect();
+    init_cards.push(PlayerCard {
+        card_type: CardType::Joker,
+        number: init_joker,
+    });
+    init_cards.shuffle(&mut rng);
+    // assign cards to players;
+    let index_of_first_action_player: usize = rng.gen_range(0..=1);
+    println!(
+        "who is going first? player{:?}",
+        index_of_first_action_player + 1
+    );
+    let cards_of_first_player: usize = match index_of_first_action_player {
+        0 => card_options.len() + 1,
+        _ => card_options.len(),
+    };
+    let player2_cards = init_cards.split_off(cards_of_first_player);
+    println!("cards of player1: {:?}", init_cards);
+    println!("cards of player2: {:?}", player2_cards);
+    return (
+        Player {
+            counter: steps,
+            cards: init_cards,
+        },
+        Player {
+            counter: steps,
+            cards: player2_cards,
+        },
+    );
+}
+
 impl Default for Hangman {
     fn default() -> Self {
         let default_jokers = vec![1, 2, 3, 4, 5];
         let default_cards = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let default_steps: usize = 11;
-        let mut rng: rand::rngs::ThreadRng = rand::thread_rng();
+        let players = new_hangman_game(&default_jokers, &default_cards, default_steps);
 
-        let init_joker = default_jokers[rng.gen_range(0..default_jokers.len())];
-        println!("joker this round: {}", init_joker);
-        // shuffle cards
-        let mut init_cards: Vec<_> = default_cards
-            .clone()
-            .into_iter()
-            .flat_map(|x| {
-                vec![
-                    PlayerCard {
-                        card_type: CardType::Number,
-                        number: x,
-                    },
-                    PlayerCard {
-                        card_type: CardType::Number,
-                        number: x,
-                    },
-                ]
-            })
-            .collect();
-        init_cards.push(PlayerCard {
-            card_type: CardType::Joker,
-            number: init_joker,
-        });
-        init_cards.shuffle(&mut rng);
-        // assign cards to players;
-        let index_of_first_action_player: usize = rng.gen_range(0..=1);
-        println!(
-            "who is going first? player{:?}",
-            index_of_first_action_player + 1
-        );
-        let cards_of_first_player: usize = match index_of_first_action_player {
-            0 => default_cards.len() + 1,
-            _ => default_cards.len(),
-        };
-        let player2_cards = init_cards.split_off(cards_of_first_player);
-        println!("cards of player1: {:?}", init_cards);
-        println!("cards of player2: {:?}", player2_cards);
         return Hangman {
             jokers: default_jokers,
             cards: default_cards,
             required_steps: default_steps,
-            players: (
-                Player {
-                    counter: default_steps,
-                    cards: init_cards,
-                },
-                Player {
-                    counter: default_steps,
-                    cards: player2_cards,
-                },
-            ),
+            players: players,
         };
     }
 }
